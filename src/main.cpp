ffbe5e40909d9eab4ee9720bd9f0a9f355b5707a
Ivan Tat <ivtat3048@gmail.com>
2026-04-17 21:24:46 -0400

    feat: implement character counting

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include "parser.hpp"
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) return 0;
+    if (argc < 2)
+    {
+        std::cout << "Usage: ./compressor [file]";
+        return 0;
+    }
 
     Parser parser(argv[1]);
+    parser.parse();
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <cstddef>
 #include "parser.hpp"
 
 void Parser::parse()
 {
+    std::ifstream file(filePath_);
 
+    if (file.is_open())
+    {
+        std::string line;
+
+        while (std::getline(file, line))
+        {
+            for (std::size_t i = 0; i < line.size(); ++i)
+            {
+                ++charFrequency_[static_cast<int>(line[i])];
+            }
+        }
+    }
 }
