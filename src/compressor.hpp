deaf159af2f4d03bfbd6108db571562e3b8be75b
Ivan Tat <ivtat3048@gmail.com>
2026-04-26 15:01:59 -0400

    refactor: simplified Compressor class. Class should be responsible for ochestrating instead of handling logic

--- src/compressor.hpp
+++ src/compressor.hpp
@@ -2,25 +2,21 @@
 #define COMPRESSOR_H
 
 #include <string>
-#include <array>
 #include <utility>
-#include <cstdint>
 
 class Compressor
 {
 
 public:
 
-    Compressor(std::string filePath, const std::array<std::uint32_t, 256>& charFrequency, const std::array<std::string, 256>& codes)
-        : filePath_(std::move(filePath)), charFrequency_(charFrequency), codes_(codes){}
+    Compressor(std::string filePath)
+        : filePath_(std::move(filePath)){}
 
     void compress();
 
 private:
 
-    std::string filePath_;
-    std::array<std::uint32_t, 256> charFrequency_{};
-    std::array<std::string, 256> codes_{};
+    std::string filePath_{};
 };
 
 #endif
