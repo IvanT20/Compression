057287dbdad6fd98833c43feea59f76682d1b7b3
Ivan Tat <ivtat3048@gmail.com>
2026-04-27 17:08:49 -0400

    feat: write frequency table header to compressed file

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -1,13 +1,27 @@
 #include <fstream>
+#include <iostream>
 #include "compressor.hpp"
+#include "parser.hpp"
+#include "huffmanTree.hpp"
 
 void Compressor::compress()
 {
+    Parser parser(filePath_);
+    parser.parse();
+    const auto& charFrequency = parser.getCharFrequency();
+    HuffmanTree tree(charFrequency);
+    std::array<std::string, 256> codes = tree.generateCodes();
+
+    std::ofstream outputFile("compressed" + filePath_, std::ios::binary);
+    outputFile.write(reinterpret_cast<const char*>(charFrequency.data()), sizeof(uint32_t) * charFrequency.size()); // Writing header information
+
     std::ifstream inputFile(filePath_);
-    std::ofstream outputFile("compressed_" + filePath_, std::ios::binary);
+    std::string buffer;
+    char c;
 
-    if (!inputFile.is_open() || !outputFile.is_open()) return;
-
-    outputFile.write(reinterpret_cast<const char*>(charFrequency_.data()), charFrequency_.size() * sizeof(std::uint32_t));
+    while (inputFile.get(c))
+    {
+        buffer += codes[static_cast<unsigned char>(c)];
+    }
 
 }
