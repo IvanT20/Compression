7c2eb0688e02ff488a371f05afb9f962692325e2
Ivan Tat <ivtat3048@gmail.com>
2026-04-27 23:13:57 -0400

    refactor!: change parse() to parseUncompressed() to match new function name

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -7,7 +7,7 @@
 void Compressor::compress()
 {
     Parser parser(filePath_);
-    parser.parse();
+    parser.parseCompressed();
     const auto& charFrequency = parser.getCharFrequency();
     HuffmanTree tree(charFrequency);
 
