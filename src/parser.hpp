09050212faf8d4be4f6fd37ee2865f284814dc87
Ivan Tat <ivtat3048@gmail.com>
2026-04-27 23:11:19 -0400

    refactor!: rename parse() to parseUncompressed() for better clarity

--- src/parser.hpp
+++ src/parser.hpp
@@ -14,13 +14,15 @@ public:
     explicit Parser(std::string filePath)
         : filePath_(std::move(filePath)){}
 
-    void parse();
+    void parseUncompressed();
+    void parseCompressed();
     const std::array<std::uint32_t, 256>& getCharFrequency() const;
 
 private:
 
     std::string filePath_;
     std::array<std::uint32_t, 256> charFrequency_{};
+    std::uint8_t padding_{};
 };
 
 #endif
