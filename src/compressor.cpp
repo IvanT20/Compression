470141a38d5251b9e722460ce721db5c1dc69e68
Ivan Tat <ivtat3048@gmail.com>
2026-04-27 21:09:52 -0400

    feat: implement bit packing in compressor

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -10,18 +10,60 @@ void Compressor::compress()
     parser.parse();
     const auto& charFrequency = parser.getCharFrequency();
     HuffmanTree tree(charFrequency);
-    std::array<std::string, 256> codes = tree.generateCodes();
 
-    std::ofstream outputFile("compressed" + filePath_, std::ios::binary);
+    if (tree.empty()) return;
+
+    std::array<std::string, 256> codes = tree.generateCodes();
+    std::ofstream outputFile("compressed_" + filePath_, std::ios::binary | std::ios::out);
+
+    if (!outputFile.is_open()) return;
+
     outputFile.write(reinterpret_cast<const char*>(charFrequency.data()), sizeof(uint32_t) * charFrequency.size()); // Writing header information
 
-    std::ifstream inputFile(filePath_);
-    std::string buffer;
+    std::ifstream inputFile(filePath_, std::ios::binary);
+
+    if (!inputFile.is_open()) return;
+
+    std::uint8_t byte = 0;
+    std::uint8_t padding = 0;
+    int bitCount = 0;
     char c;
+
+    std::streampos paddingPos = outputFile.tellp();
+    outputFile.write(reinterpret_cast<const char*>(&padding), sizeof(std::uint8_t)); // Placeholder for now since we don't know how much padding is needed
 
     while (inputFile.get(c))
     {
-        buffer += codes[static_cast<unsigned char>(c)];
+        const std::string& code = codes[static_cast<unsigned char>(c)];
+
+        for (char bit : code)
+        {
+            byte <<= 1;
+
+            if (bit == '1')
+            {
+                byte |= 1;
+            }
+
+            ++bitCount;
+
+            if (bitCount == 8)
+            {
+                outputFile.write(reinterpret_cast<const char*>(&byte), sizeof(std::uint8_t));
+                byte = 0;
+                bitCount = 0;
+            }
+        }
     }
 
+    if (bitCount > 0)   // Didn't fill full byte
+    {
+        padding = 8 - bitCount;
+        byte <<= padding;
+        outputFile.write(reinterpret_cast<const char*>(&byte), sizeof(std::uint8_t));
+    }
+
+    outputFile.clear();
+    outputFile.seekp(paddingPos, std::ios::beg);
+    outputFile.put(static_cast<char>(padding));
 }
