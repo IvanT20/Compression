6f1a92d207cf2bf3dded624d7357b1fc73ca4f18
Ivan Tat <ivtat3048@gmail.com>
2026-04-27 23:28:12 -0400

    feat: implement parseCompressed() for file decompression

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,8 +1,10 @@
 #include <fstream>
 #include <cstddef>
+#include <stdexcept>
+#include <cstring>
 #include "parser.hpp"
 
-void Parser::parse()
+void Parser::parseUncompressed()
 {
     std::ifstream file(filePath_, std::ios::binary);
 
@@ -14,6 +16,27 @@ void Parser::parse()
         {
             ++charFrequency_[static_cast<unsigned char>(c)];
         }
+    }
+}
+
+void Parser::parseCompressed()
+{
+    std::ifstream file(filePath_, std::ios::binary);
+
+    if (file.is_open())
+    {
+        char buffer[1024]{};
+        file.read(buffer, sizeof(buffer));
+
+        if (file.gcount() != 1024)
+        {
+            throw std::runtime_error("File is too small\n");
+        }
+
+
+        std::memcpy(charFrequency_.data(), buffer, charFrequency_.size() * sizeof(std::uint32_t));
+        file.read(buffer, sizeof(std::uint8_t));
+        std::memcpy(&padding_, buffer, sizeof(padding_));
     }
 }
 
