4a2c20e9d6b03cb66a4abdbac306116a9576caa1
Ivan Tat <ivtat3048@gmail.com>
2026-04-27 23:18:00 -0400

    Revert "refactor!: change parse() to parseUncompressed() to match new function name"
    This reverts commit 7c2eb0688e02ff488a371f05afb9f962692325e2.

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -7,7 +7,7 @@
 void Compressor::compress()
 {
     Parser parser(filePath_);
-    parser.parseCompressed();
+    parser.parse();
     const auto& charFrequency = parser.getCharFrequency();
     HuffmanTree tree(charFrequency);
 
