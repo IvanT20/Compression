c02059e391815411a6d75e66ada58e12d1b73096
Ivan Tat <ivtat3048@gmail.com>
2026-04-28 23:50:12 -0400

    feat: update main to now support file decompression

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
-#include "parser.hpp"
-#include "huffmanTree.hpp"
+#include <string>
 #include "compressor.hpp"
+#include "decompressor.hpp"
 
 int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        std::cout << "Usage: ./compressor [file]";
+        std::cout << "Usage: ./compressor [flags] [file]\n\n";
+        std::cout << "flags:\n-d for file decompression\nno flags for file compression\n";
         return 0;
     }
 
-    Parser parser(argv[1]);
-    parser.parse();
-    HuffmanTree tree(parser.getCharFrequency());
-    tree.createCoding();
-    tree.printTree();
-    Compressor compressor(argv[1], parser.getCharFrequency(), tree.getCodes());
-    compressor.compress();
+    if (argc == 3)
+    {
+        if (std::string(argv[1]) == "-d")
+        {
+            Decompressor decompressor(argv[2]);
+            decompressor.decompress();
+        }
+    }
+    else
+    {
+        Compressor compressor(argv[1]);
+        compressor.compress();
+    }
 }
